7a80ae973f8cbcbd249aa6246a8c2dd9baf5a9e5
mohamadsadem0 <mohamad1.sadem@gmail.com>
2024-08-29 23:05:56 +0300

    generate token

--- backend/src/main/java/com/example/ShopiShop/security/JwtService.java
+++ backend/src/main/java/com/example/ShopiShop/security/JwtService.java
@@ -3,20 +3,48 @@ package com.example.ShopiShop.security;
 
 import io.jsonwebtoken.Claims;
 import io.jsonwebtoken.Jwts;
+import io.jsonwebtoken.io.Decoders;
+import io.jsonwebtoken.security.Keys;
+import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.stereotype.Service;
+
+import java.security.Key;
+import java.util.Base64;
+import java.util.Map;
+import java.util.function.Function;
 
 @Service
 public class JwtService {
 
+    private final static String SERCRET_KEY="4a1684e3c328c385c8546fff6d46c79ef2d0581b98fff24bfd873e5c86d383d2";
+
     public String extractUsername(String token) {
-        return  null;
+        return  extractClain(token, Claims::getSubject);
+    }
+    public <T> T extractClain(String token, Function<Claims,T> claimResolver){
+         final Claims claims=extractAllClaims(token);
+         return claimResolver.apply(claims);
+    }
+
+    public String generateToken(
+        Map<String,Object> extraClaims,
+                UserDetails userDetails){
+        return Jwts.builder()
+                .setClaims(extraClaims)
+                .setSubject(userDetails.getUsername())
+                .setIssuedAt();
     }
 
     private Claims extractAllClaims(String token){
-        return Jwts.parserBuilder() .setSigningKey(getSigningKey())
-                .build()
-                .parseClaimsJws(token)
+        return Jwts.parserBuilder()
+                .setSigningKey(getSignInKey())
+                .build().parseClaimsJwt(token)
                 .getBody();
     }
+
+    private Key getSignInKey() {
+        byte[] keyBytes= Decoders.BASE64.decode(SERCRET_KEY);
+        return Keys.hmacShaKeyFor(keyBytes);
     }
+}
 
