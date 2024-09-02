70bca34f6dee2a29c5ae1ae661d7a8932dd59b60
MohamadSadem0 <mohamad1.sadem@gmail.com>
2024-09-02 08:43:28 +0300

    .

--- backend/src/main/java/com/example/ShopiShop/core/User/repository/UserRepository.java
+++ backend/src/main/java/com/example/ShopiShop/core/User/repository/UserRepository.java
@@ -1,4 +1,11 @@
 package com.example.ShopiShop.core.User.repository;
 
-public class UserRepository {
+import com.example.ShopiShop.core.User.model.User;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import java.util.Optional;
+
+public interface UserRepository extends JpaRepository<User,Long> {
+
+    Optional<User> findByEmail(String email);
 }
