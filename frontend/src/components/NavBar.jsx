12a0b2ea404150f92f745385df465c3f14c122f3
mohamadsadem0 <mohamad1.sadem@gmail.com>
2024-09-03 12:13:40 +0300

    navbar done

--- frontend/src/components/NavBar.jsx
+++ frontend/src/components/NavBar.jsx
@@ -1,6 +1,3 @@
-
-
-
 import React, { useState } from 'react';
 import Logo from '../assets/images/Logo.png';
 import SearchIcon from '../assets/icons/Search_alt_light.svg';
@@ -19,17 +16,19 @@ const Navbar = () => {
   };
 
   return (
-    <nav className="w-[95%] bg-[#585649] h-[80px] p-2 rounded-2xl shadow-lg fixed top-0 left-1/2 transform -translate-x-1/2 z-50 mt-5">
-      <div className="container mx-auto flex justify-between items-center h-full">
+    <nav className="w-[95%] bg-[#585649] h-auto p-2 rounded-2xl shadow-lg fixed top-0 left-1/2 transform -translate-x-1/2 z-50 mt-5">
+      <div className="px-10 flex h-full">
         {/* Logo Section */}
-        <img
-          className="h-12 w-auto"
-          src={Logo}
-          alt="Logo"
-        />
+        <div className="flex items-center space-x-4 mr-auto">
+          <img
+            className="h-24 w-auto"
+            src={Logo}
+            alt="Logo"
+          />
+        </div>
 
-        {/* Navigation Links */}
-        <div className="hidden md:flex space-x-8 text-white font-['Roboto'] text-lg items-center">
+        {/* Combined Navigation Section */}
+        <div className="flex items-center space-x-10 mr-auto justify-between text-white font-['Roboto'] text-lg">
           <a href="#" className="relative group">
             Home
             <span className="absolute bottom-0 left-0 w-full h-0.5 bg-[#fede00] scale-x-0 group-hover:scale-x-100 transition-transform duration-300"></span>
@@ -46,43 +45,52 @@ const Navbar = () => {
             About Us
             <span className="absolute bottom-0 left-0 w-full h-0.5 bg-[#fede00] scale-x-0 group-hover:scale-x-100 transition-transform duration-300"></span>
           </a>
+        </div>
 
+        {/* Right Section (Search Icon, Location, Map Icon, and Sign In) */}
+        <div className="relative flex items-center space-x-2">
           {/* Search Icon and Input */}
-          <div className="relative">
-            <button onClick={handleSearchToggle} className="focus:outline-none">
-              <img src={SearchIcon} alt="Search" className="h-6 w-6 text-white" />
-            </button>
+          <button onClick={handleSearchToggle} className="focus:outline-none">
+            <div className="pr-10">
+              <img
+                src={SearchIcon}
+                alt="Search"
+                className="h-6 w-6 text-white transition-transform duration-300 transform hover:scale-110"
+              />
+            </div>
+          </button>
+          <div
+            className={`relative bg-[#585649] text-white border-b border-[#fede00] focus:outline-none p-1 transition-width duration-300 ease-in-out ${
+              isSearchOpen ? 'w-48 opacity-100' : 'w-0 opacity-0'
+            }`}
+          >
             {isSearchOpen && (
               <input
                 type="text"
                 value={searchQuery}
                 onChange={handleSearchChange}
                 placeholder="Search..."
-                className="absolute left-8 top-0 bg-[#585649] text-white border-b border-[#fede00] focus:outline-none p-1"
+                className="w-full bg-transparent text-white border-none focus:outline-none"
               />
             )}
           </div>
-        </div>
 
-        {/* Right Section (Location, Map Icon, and Sign In) */}
-        <div className="flex items-center space-x-6">
-          {/* Location */}
-          <div className="flex items-center space-x-2 text-white">
-            {/* <div className="w-4 h-4 border-2 border-white rounded-full relative flex justify-center items-center">
-              <div className="w-2 h-2 border-2 border-white rounded-full"></div>
-            </div> */}
-            <span className="text-[#ff0008] text-lg">Location</span>
+          {/* Location, Map Icon, and Sign In */}
+          <div className="flex-row flex items-center space-x-36 mr-0">
+            {/* Location */}
+            <div className="flex items-center space-x-2 text-white">
+              <span className="text-[#ff0008] text-lg">Location</span>
+              {/* Map Icon */}
+              <div>
+                <img src={MapIcon} alt="Map" className="h-6 w-6 text-white" />
+              </div>
+            </div>
+
+            {/* Sign In */}
+            <a href="#" className="text-white text-lg hover:text-[#fede00] transition-colors duration-300">
+              Sign In
+            </a>
           </div>
-
-          {/* Map Icon */}
-          <div>
-            <img src={MapIcon} alt="Map" className="h-6 w-6 text-white" />
-          </div>
-
-          {/* Sign In */}
-          <a href="#" className="text-white text-lg hover:text-[#fede00] transition-colors duration-300">
-            Sign In
-          </a>
         </div>
       </div>
     </nav>
