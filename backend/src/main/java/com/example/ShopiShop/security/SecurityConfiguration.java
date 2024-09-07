eca2325a84fafb6c24e35c06b9cd05f73e16db21
MohamadSadem0 <mohamad1.sadem@gmail.com>
2024-09-07 13:23:16 +0300

    .

---
+++ backend/src/main/java/com/example/ShopiShop/security/SecurityConfiguration.java
@@ -0,0 +1,25 @@
+package com.example.ShopiShop.security;
+
+
+import lombok.RequiredArgsConstructor;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.web.SecurityFilterChain;
+
+@Configuration
+@EnableWebSecurity
+@RequiredArgsConstructor
+public class SecurityConfiguration {
+
+    @Bean
+    public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
+        http.csrf(customizer->customizer.disable());
+                http.authorizeHttpRequests(request->request.requestMatchers().permitAll())
+//                .requestMatchers()
+//                .permitAll()
+        return http.build();
+    }
+
+}
