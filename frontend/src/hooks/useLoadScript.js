2b32beb877d0c7fc9274027e1e00b797bbf12638
HelmiAbouchakra <helmi.abouchakra@gmail.com>
2024-12-01 14:08:32 +0200

    .

--- frontend/src/hooks/useLoadScript.js
+++ frontend/src/hooks/useLoadScript.js
@@ -1,5 +1,5 @@
-const { isLoaded, loadError } = useLoadScript({
-    googleMapsApiKey: process.env.REACT_APP_GOOGLE_MAPS_API_KEY, 
-    libraries: ["places"],
-  });
+// const { isLoaded, loadError } = useLoadScript({
+//     googleMapsApiKey: process.env.REACT_APP_GOOGLE_MAPS_API_KEY, 
+//     libraries: ["places"],
+//   });
   
